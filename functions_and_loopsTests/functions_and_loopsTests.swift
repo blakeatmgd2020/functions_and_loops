8c10b3a9477ab28379e7509e52b7f7c7dbb7b6fd
Blake Wallick <bwallick@hawaii.edu>
2021-12-08 10:25:10 -1000

    Initial Commit

---
+++ functions_and_loopsTests/functions_and_loopsTests.swift
@@ -0,0 +1,33 @@
+//
+//  functions_and_loopsTests.swift
+//  functions_and_loopsTests
+//
+//  Created by Blake Wallick on 12/8/21.
+//
+
+import XCTest
+@testable import functions_and_loops
+
+class functions_and_loopsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
